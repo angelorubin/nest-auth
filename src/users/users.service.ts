350bded70918fe67913a0459df96debb4c79e168
Angelo Rubin <angelorubin@gmail.com>
2021-10-19 17:57:39 -0300

    Implementing Passport strategies

---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+}
